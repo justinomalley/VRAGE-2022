96d408229fb9ba004fcac835cac43dee758775d7
Justin O'Malley <justin@yarncorporation.com>
2022-01-20 21:48:43 -0500

    Add Chelsee and Makenna's galleries

---
+++ Assets/CMGallery.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CMGallery : GalleryRoom {
+
+    [SerializeField]
+    private AudioClip chelseeSound, makennaSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
