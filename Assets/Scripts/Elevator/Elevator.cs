54f8f2be44eb73a8e219055da2184438e8704a24
Justin O'Malley <justin@yarncorporation.com>
2022-01-23 18:18:07 -0500

    Organize the project more efficiently

---
+++ Assets/Scripts/Elevator/Elevator.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+
+/// <summary>
+/// Elevator manages the elevator that allows players to navigate between the various galleries.
+/// </summary>
+public class Elevator : MonoBehaviour {
+    
+    private ElevatorTeleportPad teleportPad;
+    
+    /* Reorientation */
+
+    private static Quaternion originalRotation;
+
+    // makennaPosition is the position the elevator should move to when in Makenna's gallery.
+    private static Vector3 originalPosition, makennaPosition;
+    
+    [SerializeField]
+    private Transform makennaTransform;
+    
+    private TransformPositionFader fader;
+
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip[] floorSounds;
+
+    private ElevatorDisplay display;
+    
+    private static bool goneAway;
+
+    [SerializeField]
+    private AnimationCurve curve;
+
+    [SerializeField]
+    private ElevatorButton[] floorButtons;
+    
+    private static Elevator instance;
+
+    private void Awake() {
+        originalRotation = transform.rotation;
+        originalPosition = transform.position;
+        makennaPosition = makennaTransform.position;
+        teleportPad = GetComponentInChildren<ElevatorTeleportPad>();
+        fader = GetComponent<TransformPositionFader>();
+        fader.SetAnimationCurve(curve);
+        audioSource = GetComponent<AudioSource>();
+        display = GetComponentInChildren<ElevatorDisplay>();
+        instance = this;
+    }
+
+    public static bool InElevator() {
+        return instance.teleportPad.IsCurrentPad();
+    }
+
+    public static void ResetPositionAndRotation() {
+        instance.transform.rotation = originalRotation;
+        instance.transform.position = new Vector3(originalPosition.x, instance.transform.position.y, originalPosition.z);
+    }
+
+    public static void RotateForOS() {
+        instance.transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+    
+    public static void RotateAndPositionForMakenna() {
+        instance.transform.rotation = Quaternion.Euler(0, 270, 0);
+        instance.transform.position = new Vector3(makennaPosition.x, instance.transform.position.y, makennaPosition.z);
+    }
+
+    public static void SelectFloor(GalleryLoader.Room room) {
+        if (GalleryLoader.GetSelectedRoom() == room || instance.display.IsLoading()) {
+            return;
+        }
+        
+        instance.audioSource.Stop();
+        instance.audioSource.clip = instance.floorSounds[(int) room];
+        instance.audioSource.Play();
+        
+        for (var i = 0; i < instance.floorButtons.Length; i++) {
+            var button = instance.floorButtons[i];
+            if (button.GetRoom() == room) {
+                instance.floorButtons[i].Select();
+            } else {
+                instance.floorButtons[i].Deselect();
+            }
+        }
+        
+        instance.display.SetRoom(room);
+        GalleryLoader.SetRoom(room);
+    }
+
+    public static Transform GetTransform() {
+        return instance.transform;
+    }
+
+    public static void GoAway(Vector3 target) {
+        if (goneAway) {
+            return;
+        }
+        goneAway = true;
+        instance.fader.Fade(target, ElevatorDoors.OpenStatic);
+    }
+
+    public static void ComeBack() {
+        if (!goneAway || instance == null) {
+            return;
+        }
+        goneAway = false;
+        instance.fader.Fade(originalPosition);
+        
+        // This call will be ignored if we aren't in the CM gallery.
+        CMGallery.DeactivateTeleportPadsOverElevator();
+    }
+}
