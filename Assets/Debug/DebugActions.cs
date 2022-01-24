e3ec4fd27ba5193834f7ff2caa492b5dc7571aec
Justin O'Malley <justin@yarncorporation.com>
2022-01-23 22:47:05 -0500

    Add materials that didn't appear in git previously

--- Assets/Debug/DebugActions.cs
+++ Assets/Debug/DebugActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/New/DebugActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Debug/DebugActions.inputactions'
 
 using System;
 using System.Collections;
